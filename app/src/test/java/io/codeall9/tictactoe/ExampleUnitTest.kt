98aecfbdad86863ae787f422aa7577c21d75c952
codeall9 <11408319+codeall9@users.noreply.github.com>
2021-09-04 18:05:00 +0800

    Setup Junit 5

--- app/src/test/java/io/codeall9/tictactoe/ExampleUnitTest.kt
+++ app/src/test/java/io/codeall9/tictactoe/ExampleUnitTest.kt
@@ -1,17 +1,18 @@
 package io.codeall9.tictactoe
 
-import org.junit.Test
-
-import org.junit.Assert.*
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@DisplayName("Example Unit Test")
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        Assertions.assertEquals(4, 2 + 2)
     }
 }
